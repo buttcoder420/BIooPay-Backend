00a7e14cba1eb84e40f29c43503f78a0e796405e
Coder Bhai <yadspakistan.pk.@gmail.com>
2025-04-21 09:12:18 +0500

    Update AdvanceModel.js

--- Model/AdvanceModel.js
+++ Model/AdvanceModel.js
@@ -4,7 +4,7 @@ const advancePromotionSchema = new mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "users",
       required: true,
     },
     teamSizeAtApplication: {
